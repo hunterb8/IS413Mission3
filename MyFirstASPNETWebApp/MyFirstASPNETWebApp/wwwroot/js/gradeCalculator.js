be407cdc1e5e61a627474e7ba6a63e8889a30ba0
hunterb8 <brittan.hunter@gmail.com>
2022-01-19 23:50:55 -0700

    push

---
+++ MyFirstASPNETWebApp/MyFirstASPNETWebApp/wwwroot/js/gradeCalculator.js
@@ -0,0 +1,7 @@
+﻿$("#buttonCalc").click(function () {
+    alert("Final Grade: "($("#assignmentGrade").val() * 0.55 +
+    $("#projectGrade").val() * 0.05 + 
+    $("#quizGrade").val() * 0.1 +
+    $("#examGrade").val() * 0.20 +
+    $("#intexGrade").val() * 0.1).toFixed(2) + "%");
+})
